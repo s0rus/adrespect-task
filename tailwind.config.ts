8b66403bb23ea6f3865289d62b96ba2306924f40
s0rus <piotrmolkontakt@gmail.com>
2023-08-14 19:01:51 +0200

    ✨ initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{html,ts}', './*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
